33396ddbd25cf6c00ba75029e335ac9de509b91f
Le Trong Dat <datlt2306@gmail.com>
2023-02-06 17:00:53 +0700

    feat: Sử dụng navigojs

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
+
+const app = document.querySelector("#app");
+import AboutPage from "./pages/about";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFound";
+import ProductsPage from "./pages/Products";
+import { render, router } from "./utilities";
+
+router.on("/", () => render(HomePage, app));
+router.on("/about", () => render(AboutPage, app));
+router.on("/products", () => render(ProductsPage, app));
+router.notFound(() => render(NotFoundPage, app));
+router.resolve();
--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,1 +1,7 @@
-export const myName = "Le Trong Dat";
+import Navigo from "navigo";
+const router = new Navigo("/", { linksSelector: "a", hash: true });
+
+const render = (content, target) => {
+    target.innerHTML = content();
+};
+export { render, router };
